4ea743338ac5f70e1ebc5699dcdcba5caa940949
Anton Osenenko <anton.osenenko@gmail.com>
2015-01-20 11:16:12 +0300

    update code

--- uploader.rb
+++ uploader.rb
@@ -1,15 +1,27 @@
+require 'rubygems'
+require 'action_view'
 require 'elasticsearch'
 require 'active_support/all'
+require 'active_support/number_helper'
 require 'nokogiri'
 require 'zip'
 require 'pp'
 
 class Flibusta
-  def initialize
-    @elastic = Elasticsearch::Client.new log: false
-    @elastic.transport.reload_connections!
+  include ActionView::Helpers::NumberHelper
 
-    @bulk_size = ENV['SIZE'].present? ? ENV['SIZE'].to_i : 37
+  def initialize
+    @elastic = Elasticsearch::Client.new log: false,
+                                         :retry_on_failure => 10,
+                                         :reload_connections => 100,
+                                         :transport_options => {
+                                             :request => {
+                                                 :timeout => 60,
+                                                 :open_timeout => 30
+                                             }
+                                         }
+
+    @bulk_size = ENV['SIZE'].present? ? ENV['SIZE'].to_i : 53
   end
 
   def process_fb2(content)
@@ -18,58 +30,6 @@ class Flibusta
     content = doc.search('body').first.text.unpack('U*').pack('U*').gsub(/\s+/, ' ').strip rescue nil
     description = doc.search('description').first.text.unpack('U*').pack('U*').gsub(/\s+/, ' ').strip rescue nil
     [content, description]
-  end
-
-  def upload_bulk(zip_file, fb_files, global_index, global_total)
-    bulk_body = []
-
-    total = fb_files.size
-    fb_files.each_with_index do |fb_file, index|
-      unless fb_file.end_with?('.fb2')
-        puts "Not fb2 #{fb_file}"
-        next
-      end
-
-      id = File.basename(fb_file).chomp('.fb2')
-
-      raw = File.read(fb_file)
-      content, description = process_fb2(raw)
-
-      bulk_body << {
-          index: {
-              _index: 'books',
-              _type: 'book',
-              _id: id,
-              data: {
-                  content: content,
-                  description: description,
-                  zip_file: zip_file
-              }
-          }
-      }
-
-      puts "#{index + global_index}(#{total})/#{global_total}"
-    end
-
-    puts 'Upload bulk .. '
-    result = @elastic.bulk body: bulk_body, timeout: 120, replication: 'async', refresh: false
-    if result['errors']
-      puts result
-    end
-    nil
-  end
-
-  def upload_dir(dir)
-    path = File.expand_path(dir)
-    zip_file = File.basename(path)
-    files = Dir[path + '/*'].map { |e| e }
-    total = files.size
-    uploaded = 0
-    files.each_slice(@bulk_size) do |slice_files|
-      upload_bulk(zip_file, slice_files, uploaded, total)
-      uploaded += slice_files.size
-      puts "#{uploaded}/#{total}"
-    end
   end
 
   def upload_folder_with_zips(start_folder)
@@ -96,6 +56,7 @@ class Flibusta
 
           raw_zip = entry.get_input_stream.read
           content, description = process_fb2(raw_zip)
+          human_size = number_to_human_size(content.size)
           zip_file_name = File.basename(zip_file)
           bulk_body << {
               index: {
@@ -110,7 +71,8 @@ class Flibusta
               }
           }
 
-          puts "id:#{id} #{zip_file} (#{zip_file_index}/#{zip_files_count}) -> #{entry.name} (#{entry_index}/#{entries_count})"
+
+          puts "id:#{id} #{human_size} #{zip_file} (#{zip_file_index}/#{zip_files_count}) -> #{entry.name} (#{entry_index}/#{entries_count})"
         end
 
         if bulk_body.present?
@@ -125,5 +87,4 @@ class Flibusta
 end
 
 fb = Flibusta.new
-# fb.upload_dir(ARGV[0])
 fb.upload_folder_with_zips(ARGV[0])
